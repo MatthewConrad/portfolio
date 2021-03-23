7af2a5f57bc202169842ae7e198d6d91977da808
Matthew Conrad <conrad.matthewj@gmail.com>
2021-03-23 19:54:30 -0400

    Added some hero content and styling. Changed weird prettier rule so that things have semicolons.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,29 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import SEO from "../components/seo"
+import * as React from "react";
+import SEO from "../components/seo";
+import Blobs from "../components/blobs";
 
 const IndexPage = () => (
   <>
-    <SEO title="Full-stack Developer" />
-    <h1>Hi, I&rsquo;m Matthew.</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <div className="content-wrapper">
+      <SEO title="Full-stack Developer" />
+      <h1>Hi, I&rsquo;m Matthew.</h1>
+      <h2>I'm a full-stack developer.</h2>
+      <p>
+        In my 7 years of industry experience I've worked on everything from
+        gamification platforms
+        <br /> to self-service chatbots. I've got a passion for front-end
+        development and design,
+        <br /> picking up new technologies, and teaching others.
+      </p>
+      <p>
+        When I'm not writing code, I'm a competitive marathoner with dreams of
+        completing
+        <br /> all six World Marathon Majors and making it to the Olympic
+        Trials. I also love baking and <br />
+        photography, and am currently learning Italian and French.
+      </p>
+    </div>
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;
