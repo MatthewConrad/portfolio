33ae7e3b633238ab2d3a0ffa032870951845dbc7
Matthew Conrad <conrad.matthewj@gmail.com>
2021-03-28 20:05:07 -0400

    Adjusted speeds for parallax on the blobs and added scroll movement.

--- src/components/blobs.js
+++ src/components/blobs.js
@@ -3,6 +3,8 @@ const Blobs = () => {
   const blobs = [];
   const minRadius = 35;
   const maxRadius = 65;
+  const speeds = [1, -2, 3];
+  const topPositions = [-96, 288, 48];
 
   const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
   const radiusPropReducer = (val, radius) =>
@@ -19,21 +21,22 @@ const Blobs = () => {
       `${radiusesA.reduce(radiusPropReducer, "")}` +
       `/ ${radiusesB.reduce(radiusPropReducer, "").trim()}`;
 
-    const speed = [2, -4, 6];
     const style = {
       borderRadius: borderRadiuses,
+      top: `${topPositions[i]}px`,
     };
     blobs.push(
       <div
         className="blob"
         id={`blob${i + 1}`}
+        key={i}
         style={style}
-        data-speed={speed[i]}
+        data-speed={speeds[i]}
       ></div>
     );
   }
 
-  const parallax = event => {
+  const parallaxMovement = event => {
     document.querySelectorAll(".blob").forEach(blob => {
       const speed = blob.getAttribute("data-speed");
       const x = (window.innerWidth - event.pageX * speed) / 100;
@@ -43,7 +46,24 @@ const Blobs = () => {
     });
   };
 
-  document.addEventListener("mousemove", parallax);
+  const scrollMovement = event => {
+    const maxPositionBasis = topPositions[1];
+    const currentScroll = window.scrollY;
+    const maxScroll =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const percentageScroll = currentScroll / maxScroll;
+
+    document.querySelectorAll(".blob").forEach((blob, index) => {
+      const top = topPositions[index];
+      const maxTop = window.innerHeight - 48 - maxPositionBasis;
+      blob.style.top = `${
+        top + (maxTop - maxPositionBasis) * percentageScroll
+      }px`;
+    });
+  };
+
+  document.addEventListener("mousemove", parallaxMovement);
+  document.addEventListener("scroll", scrollMovement);
 
   return <div className="blobs">{blobs}</div>;
 };
